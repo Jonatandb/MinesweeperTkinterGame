1f8a107713f2b3b4d66098ceb2b61b7ca39055e2
Jonatandb <jonatandb@gmail.com>
2022-04-22 22:20:01 -0300

    Basic UI frames added

--- minesweeper.py
+++ minesweeper.py
@@ -9,5 +9,22 @@ root.configure(bg='black')
 root.title("Minesweeper Game")
 root.resizable(False, False)
 
+top_frame = Frame(
+    root,
+    bg='red',  # Change later to black
+    width=1440,
+    height=180
+)
+top_frame.place(x=0, y=0)
+
+left_frame = Frame(
+    root,
+    bg='blue',  # Change later to black
+    width=360,
+    height=540
+)
+left_frame.place(x=0, y=180)
+
+
 # Run the window
 root.mainloop()
