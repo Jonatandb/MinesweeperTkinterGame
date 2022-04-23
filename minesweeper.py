009b1270bf1751a63d628049eed31e205a8c3342
Jonatandb <jonatandb@gmail.com>
2022-04-22 22:17:44 -0300

    Settings file added

--- minesweeper.py
+++ minesweeper.py
@@ -1,8 +1,13 @@
 from tkinter import *
+import settings
 
 root = Tk()
-root.geometry('1440x720')
+
+# Override the settings of the window
+root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
 root.configure(bg='black')
 root.title("Minesweeper Game")
 root.resizable(False, False)
+
+# Run the window
 root.mainloop()
